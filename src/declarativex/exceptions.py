a6a400621f07577770ff547e8837be23c1fa6434
Vladyslav Halatskyi <xfloydya@gmail.com>
2023-09-13 23:39:22 +0300

    fix: Name accessor for type hints (py38, py39)

--- src/declarativex/exceptions.py
+++ src/declarativex/exceptions.py
@@ -1,5 +1,5 @@
 from collections.abc import Iterable
-from typing import Type, Sequence, Union, Mapping, Optional, TYPE_CHECKING
+from typing import Type, Sequence, Union, Mapping, Optional, TYPE_CHECKING, Any
 
 import httpx
 
@@ -18,22 +18,33 @@ class MisconfiguredException(DeclarativeException):
 
 
 class DependencyValidationError(DeclarativeException):
+
+    @classmethod
+    def _name_accessor(cls, entity: Any) -> str:
+        if hasattr(entity, "__name__"):
+            return entity.__name__
+        if hasattr(entity, "__class__"):
+            return cls._name_accessor(entity)
+        return str(entity)
+
     def __init__(
         self, expected_type: Union[Type, Sequence[Type]], received_type: Type
     ):
         self._is_none = isinstance(None, received_type)
         message = f"Value of type {received_type.__name__} is not supported. "
         if isinstance(expected_type, Iterable):
+            names = [self._name_accessor(arg) for arg in expected_type]
             message += (
-                f"Expected one of: {[arg.__name__ for arg in expected_type]}."
+                f"Expected one of: {names}."
             )
         else:
-            message += f"Expected type: {expected_type.__name__}."
+            message += f"Expected type: {self._name_accessor(expected_type)}."
         if self._is_none:
+            names = [self._name_accessor(type_) for type_ in expected_type]
             type_hint = (
-                f"Union{[type_.__name__ for type_ in expected_type]}"
+                f"Union{names}"
                 if isinstance(expected_type, Iterable)
-                else expected_type.__name__
+                else self._name_accessor(expected_type)
             )
             message += (
                 " Specify a default value or "
